0afb02e08e0cd88578894a14deb28098681ce05a
alexandrmotologa <al.motologa@gmail.com>
2020-11-09 13:12:56 +0200

    hw

--- hw-magic/main.py
+++ hw-magic/main.py
@@ -1,23 +1,21 @@
-# class RandomInput:
-#     def __init__(self):
-#         self.n = 0
-    
-#     def __next__(self):
-#         self.n +=2
-#         self.count=0
-#         if self.count <= 9:
-#             return self.n
-#         else:
-#             raise StopIteration
-    
-#     def __iter__(self):
-#         return self
-
-# c_object = RandomInput()
-# for e in c_object:
-#     print(e)
-
-
 import random
-for x in range(10):
-    print (random.randint(1,1000))
+
+class RandomInteger:
+    def __init__(self):
+        self.n = 0
+        self.count=0
+    
+    def __next__(self):
+        self.n =random.randint(1,1000)
+        if self.count <= 9:
+            self.count+=1
+            return self.n
+        else:
+            raise StopIteration
+    
+    def __iter__(self):
+        return self
+
+ri = RandomInteger()
+for i in ri:
+    print(i)
