405b70a25ec65b5b8bb33aa987320115b0b5410d
alexandrmotologa <al.motologa@gmail.com>
2020-11-14 01:32:35 +0200

    hw

--- proxy-image-processor/app.py
+++ proxy-image-processor/app.py
@@ -1,29 +1,37 @@
-from os import path, listdir
+from os import path, listdir, walk
 from time import sleep
 import collections
 
 
 def observe(directory):
+
+    def sort_files():
+        """Sort the files separately from the folders"""
+        file_sort = []
+        for root, dirs, files in walk(directory):
+            for file in files:
+                file_sort.append(file)
+            return file_sort
+
     # check if path exists
     if not path.isdir(directory):
         print("ERROR - directory not found!!!")
         return
-
-    last_file_list = listdir(directory)
-
+    else:
+        sort_files()
+    # start watching
+    last_file_list = sort_files()
     while True:
         ext = [".jpg", ".jpeg"]
-        file_list = listdir(directory)
+        file_list = sort_files()
 
         if collections.Counter(last_file_list) != collections.Counter(file_list):
             added = [f for f in file_list if not f in last_file_list]
             removed = [f for f in last_file_list if not f in file_list]
 
             if added and added[0].endswith(tuple(ext)) and path.isfile(directory + "/" + added[0]): print(
-                f"New File {added} Added in folder {directory}")
-            if removed and removed[0].endswith(tuple(ext)): print(f"File {removed} Removed from folder {directory}")
-
-            last_file_list = file_list
+                f"New File {added[0]} Added in folder {directory}")
+            if removed and removed[0].endswith(tuple(ext)): print(f"File {removed[0]} Removed from folder {directory}")
 
         last_file_list = file_list
         sleep(5)
